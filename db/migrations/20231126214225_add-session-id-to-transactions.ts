5d1702a0b45bdcef58bc584b619026b0187f9560
joao <joaopaulo94.economia@gmail.com>
2023-11-26 18:51:30 -0300

    Migrations - criação de um novo campo na tabela

---
+++ db/migrations/20231126214225_add-session-id-to-transactions.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex'
+// criando um novo campo
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.uuid('session_id').after('id').index()
+  })
+}
+// deletar o campo caso necessário
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropColumn('session_id')
+  })
+}
