bce9a2498574931a9a1dac49fd33efee553c654b
joao <joaopaulo94.economia@gmail.com>
2023-11-28 23:10:45 -0300

    Recuperando o arquivo de variáveis de ambiente

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,16 @@
+// ler o arquivo com as vars de ambiente
+import 'dotenv/config'
 // conexão com o banco de dados
 import { knex as setupKnex, Knex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL env not found.')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
