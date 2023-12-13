7041016895c145a0ceb161f85609fe85eb280507
joao <joaopaulo94.economia@gmail.com>
2023-12-13 07:34:20 -0300

    Testes automatizados - Categorizando os testes

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,24 +1,28 @@
-import { expect, test, beforeAll, afterAll } from 'vitest'
+import { expect, test, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-// função executada antes de todos os testes, 1 única vez
-beforeAll(async() => {
-    await app.ready()
-})
 
-afterAll(async() => {
-    await app.close()
-})
-
-test('User can create a new transaction', async() => {
-    const response = await request(app.server)
-    .post('/transactions')
-    .send({
-        title: 'New transaction',
-        amount: 5000,
-        type: 'credit'
+describe('Transactions routes', () => {
+    // função executada antes de todos os testes, 1 única vez
+    beforeAll(async () => {
+        await app.ready()
     })
 
-    expect(response.statusCode).toEqual(201)
+    afterAll(async () => {
+        await app.close()
+    })
+
+    test('User can create a new transaction', async () => {
+        const response = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+
+        expect(response.statusCode).toEqual(201)
+    })
 })
+
