f9be9b7c290848baf7ecc6324d55d495e7c575a8
joao <joaopaulo94.economia@gmail.com>
2024-01-08 21:06:52 -0300

    fix: deploy do app no Render

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ app
   .listen({
     port: env.PORT,
     // 'listen' retorna uma promise / 'then' espera a execução da promise e retorna a mensagem de servidor ok
+    host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('HTTP server Running!')
